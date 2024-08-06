8890a81015972cfa5a3bf29c095fdcd4d5a0df0a
Esteban Briceño Peroza <162641471+00000Esteban1111@users.noreply.github.com>
2024-08-06 18:33:37 +0000

    fix: resolicion de problemas iniciales

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,16 +1,27 @@
 import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import { Context } from "../store/appContext.js";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const handleImgError = (event) => {
+		// console.log(event.target.src);
+		event.target.src = 'https://starwars-visualguide.com/assets/img/placeholder.jpg'
+
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			{/* <Alert/> */}
+			{/* <User /> */}
+			{/* <Jumbotron/> */}
+				
+ 			<h1>Hello Rigo!!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={'https://starwars-visualguide.com/assets/img/planets/1.jpg'} 
+						onError={handleImgError}/>
 			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
@@ -21,6 +32,7 @@ export const Home = () => {
 					Read documentation
 				</a>
 			</p>
+			
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,25 +2,19 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			demo: [{title: "FIRST", background: "white", initial: "white"},
+				     {title: "SECOND", background: "white", initial: "white"}],
+			cohorte: 'Spain 72',
+			user: 'spain-72',
+			host: 'https://playground.4geeks.com/contact',
+			alert: {visible: false, back: 'danger', text: 'Mensaje del back'},
+			contacts: [],
+			currentContact: null,
+			currentUser: null,
+			isLoged: false
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
+			exampleFunction: () => {getActions().changeColor(0, "green");},
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
@@ -33,10 +27,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
+			changeColor: (index, color) => { 
+				const store = getStore();  // get the store
 				//we have to loop the entire demo array to look for the respective index
 				//and change its color
 				const demo = store.demo.map((elm, i) => {
@@ -46,7 +38,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+			getUsers: async () => {
+				const url = 'https://jsonplaceholder.typicode.com/users';
+				// const url = `${getStore().host}/agendas/${getStore().user}/contacts`;
+				const options = {
+					method: 'GET'
+				};
+				const response = await fetch(url, options);
+				if (!response.ok) {
+					// Tratar el error
+					console.log('error: ', response.status, response.statusText);
+					return;
+				}
+				const data = await response.json();
+				console.log(data);  // imprimo en consola el json (el body de response) que me devuelve el fetch()
+				// setStore({ contacts: data.contacts});
+				setStore({ contacts: data});
+				localStorage.setItem('contacts', JSON.stringify(data));
+				localStorage.setItem('usuario', getStore().user)
+				// La lógica de la app
+			},
+			getPosts: () => {},
+			setAlert: (newAlert) => {setStore({ alert: newAlert})},
+			setCurrentContact: (contact) => {setStore({ currentContact: contact })},
+			setCurrentUser: (user) => {setStore({ currentUser: user })},
+			setIsLoged: (isLogin) => {setStore({ isLoged: isLogin })}
 		}
 	};
 };
